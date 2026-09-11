1b15c1575748c342304e974f8143937582f7c718
amin.rzzdh <94660683+Amindada021@users.noreply.github.com>
2026-09-11 16:13:17 +0330

    Add OdinCore source files

---
+++ IOdin.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace OdinCore;
+
+public interface IOdin<TRequest, TResponse>
+{
+    Result<TResponse> RunExecute(TRequest? request);
+
+    Task<Result<TResponse>> RunExecuteAsync(TRequest? request);
+
+    Task<Result<TResponse>> RunExecuteAsync(TRequest? request, CancellationToken cancellationToken);
+}
