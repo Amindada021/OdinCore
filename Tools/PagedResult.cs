0df3199f5b6b8558dd5e7431e5860740fce9edb1
amin.rzzdh <94660683+Amindada021@users.noreply.github.com>
2026-09-11 16:16:58 +0330

    Add paging tools

---
+++ Tools/PagedResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OdinCore.Tools
+{
+    public class PagedResult<T> : PagedResult where T : class
+    {
+        public T[] items { get; set; } = System.Array.Empty<T>();
+    }
+
+    public abstract class PagedResult
+    {
+        public int page { get; set; }
+
+        public int pageSize { get; set; }
+
+        public int total { get; set; }
+
+        public int pageCount => (int)Math.Ceiling((double)total / (double)pageSize);
+
+        public int firstIndex => (page - 1) * pageSize + 1;
+
+        public int lastIndex => Math.Min(page * pageSize, total);
+
+        public Dictionary<string, decimal>? BroughtForwardSums { get; set; }
+        public Dictionary<string, decimal>? TotalSums { get; set; }
+    }
+
+}
