ec486dccb02dd58ff36d13e65305c37b13991413
amin.rzzdh <94660683+Amindada021@users.noreply.github.com>
2026-09-11 16:14:09 +0330

    Add OdinCore source files

---
+++ ServiceLifetimeAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OdinCore;
+
+public class ServiceLifetimeAttribute : Attribute
+{
+    private readonly ServiceLifetime _serviceLifetime;
+
+    internal ServiceLifetime ServiceLifetime => _serviceLifetime;
+
+    public ServiceLifetimeAttribute(ServiceLifetime serviceLifetime)
+    {
+        _serviceLifetime = serviceLifetime;
+    }
+}
